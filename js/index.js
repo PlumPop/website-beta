0837e1c615d1d4580fd25bd336cd4ef669cb63f5
Jason Katsampes <jason.katsampes@plumpop.com>
2021-01-23 00:17:20 -0800

    Updated the participating vendor section logos to display in a carousel on the consumer page.

--- js/index.js
+++ js/index.js
@@ -4,6 +4,10 @@ const PLUMPOP_APP_STORE_URL = "https://apps.apple.com/us/app/super-mario-run/id1
 const ANDROID_USER_AGENT_REGEX = /android/i;
 const PLUMPOP_GOOGLE_PLAY_URL = "https://play.google.com/store/apps/details?id=com.nintendo.zara&hl=en_US&gl=US";
 
+const SWIPER_CONTAINER_QUERY_SELECTOR = "div.swiper-container";
+
+const VENDOR_LOGO_WIDTH = 250;
+
 function initDownloadAppButtonSmallScreen() {
     let showButton = false;
     const downloadAppButton = document.querySelector("a.download-app-small");
@@ -33,4 +37,68 @@ function initDownloadAppButtonsMediumScreen() {
 window.addEventListener("DOMContentLoaded", function () {
     initDownloadAppButtonSmallScreen();
     initDownloadAppButtonsMediumScreen();
+});
+
+function getNumSlidesPerView(windowWidth) {
+    let numSlidesPerView;
+
+    if (windowWidth < 540) {
+        numSlidesPerView = 1;
+    } else if (windowWidth < 840) {
+        numSlidesPerView = 2;
+    } else if (windowWidth < 1100) {
+        numSlidesPerView = 3;
+    } else {
+        numSlidesPerView = 4;
+    }
+
+    return numSlidesPerView;
+}
+
+function resizeSwiperContainerPadding() {
+    let $swiperContainerContainer = $("div#swiper-container-container");
+    const swiperContainerContainerWidth = $swiperContainerContainer.width();
+
+    const windowWidth = $(window).width();
+    const numSlidesPerView = getNumSlidesPerView(windowWidth);
+
+    const slidesWidth = numSlidesPerView * VENDOR_LOGO_WIDTH;
+
+    const swiperContainerElement = document.querySelector(SWIPER_CONTAINER_QUERY_SELECTOR);
+    const spaceBetweenSlides = (swiperContainerContainerWidth - slidesWidth) / (numSlidesPerView + 1);
+    swiperContainerElement.style.paddingLeft = spaceBetweenSlides + "px";
+}
+
+function initSwiper() {
+    const swiper = new Swiper(SWIPER_CONTAINER_QUERY_SELECTOR, {
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        speed: 1500,
+        loop: true,
+        slidesPerView: getNumSlidesPerView(0),
+        breakpoints: {
+            540: {
+                slidesPerView: getNumSlidesPerView(540),
+            },
+            840: {
+                slidesPerView: getNumSlidesPerView(840),
+            },
+            1100: {
+                slidesPerView: getNumSlidesPerView(1100)
+            }
+        },
+        grabCursor: true
+    });
+
+    resizeSwiperContainerPadding();
+
+    swiper.on("resize", function () {
+        resizeSwiperContainerPadding();
+    });
+}
+
+$(document).ready(function () {
+    initSwiper();
 });
